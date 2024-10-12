9a7ffe05e3c8fa243516f506758e03239c0411b4
grugwilson <emmerson.cockburn12@outlook.com>
2024-10-12 11:56:07 -0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+
+sf::CircleShape drawCircle(float size, sf::Color colour, float posX, float posY)
+{
+    // draw a circle!!!
+    sf::CircleShape circle(size);
+    // set the shape color to green
+    circle.setFillColor(colour);
+    // sets position of shape
+    circle.setPosition(posX / 2, posY);
+    // draws shape!!!
+    return circle;
+}
+
+
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    unsigned int windowWidth = 500u;
+    unsigned int windowHeight = 500u;
+    auto window = sf::RenderWindow{ { windowWidth, windowHeight}, "CMake SFML Project" };
     window.setFramerateLimit(144);
 
     while (window.isOpen())
@@ -13,9 +30,32 @@ int main()
             {
                 window.close();
             }
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.scancode == sf::Keyboard::Scan::Escape)
+                {
+                    window.close();
+                }
+            }
+
+
         }
 
-        window.clear();
+        window.clear(sf::Color::Black);
+
+        window.draw(drawCircle(50.f, sf::Color::Green, windowWidth/2 - 25.f, windowHeight/2 -25.f));
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                sf::RectangleShape rect(sf::Vector2f(50.f, 50.f));
+                rect.setFillColor(sf::Color(0,0,0));
+                rect.setOutlineThickness(-5.f);
+                rect.setOutlineColor(sf::Color(50,50,50));
+                rect.setPosition(50.f*i,50.f*j);
+                window.draw(rect);
+            }
+        }
         window.display();
     }
 }
